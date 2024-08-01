e2431d99b154f40512ba406502428155a83c3b8f
Richard Raya <rdxzv.dev@gmail.com>
2024-08-01 20:49:06 -0300

    adreno_tz: Boost GPU target frequency at any refresh rate
    Change-Id: Ia888f618686bc4a599b6fcbaf88ae69557b67eec
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -383,7 +383,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
-		if (refresh_rate > 60)
+		if (refresh_rate > 30)
 			scm_data[2] = priv->bin.busy_time * CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI / 100;
 		else
 			scm_data[2] = priv->bin.busy_time;
